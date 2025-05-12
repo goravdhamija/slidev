b615f9c4c77409500960f464baae63e2268a6d4a
goravdhamija <gorav.dhamija@gmail.com>
2025-05-12 17:20:51 +0530

    initial commit 3

--- app/src/main/java/com/globewaystechnologies/slidevideospy/MainActivity.kt
+++ app/src/main/java/com/globewaystechnologies/slidevideospy/MainActivity.kt
@@ -1,47 +1,135 @@
 package com.globewaystechnologies.slidevideospy
 
+import android.Manifest
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.globewaystechnologies.slidevideospy.ui.theme.SlideVideoSPYTheme
 
+
+
 class MainActivity : ComponentActivity() {
+
+
+    lateinit var mService: PinkService
+    var mBound: Boolean = false
+
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance.
+            val binder = service as PinkService.PinkServiceBinder
+            mService = binder.getService()
+            mBound = true
+            Log.d("PinkService", "On Service Connected" )
+
+
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+            Log.d("PinkService", "On Service Disconnected" )
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+      /* var serviceIntent = Intent(this, PinkService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }*/
+
+      //  startForegroundService(serviceIntent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+       /* unbindService(connection)
+        mBound = false*/
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val serviceIntent = Intent(this, PinkService::class.java)
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
+
             SlideVideoSPYTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column {
+                        FilledButtonStartForground {
+
+                            startForegroundService(serviceIntent)
+
+                        }
+
+                        FilledButtonStopForground {
+
+                            stopService(serviceIntent)
+
+                        }
+                    }
                 }
             }
         }
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS,  Manifest.permission.FOREGROUND_SERVICE_CAMERA, Manifest.permission.CAMERA),200)
+
+
+    }
+}
+
+
+@Composable
+fun FilledButtonStartForground(onClick: () -> Unit) {
+    Button(onClick = { onClick() }) {
+        Text("Start Foreground")
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SlideVideoSPYTheme {
-        Greeting("Android")
+fun FilledButtonStopForground(onClick: () -> Unit) {
+    Button(
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Red,
+            contentColor = Color.Black)
+    ) {
+        Text("Stop Foreground")
     }
 }
+
+
+
--- app/src/main/java/com/globewaystechnologies/slidevideospy/PinkService.kt
+++ app/src/main/java/com/globewaystechnologies/slidevideospy/PinkService.kt
@@ -1,12 +1,112 @@
 package com.globewaystechnologies.slidevideospy
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import java.util.Random
+import kotlin.concurrent.thread
 
 class PinkService : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+
+    private var allowRebind: Boolean = false
+
+    private val binder: PinkServiceBinder by lazy {
+        PinkServiceBinder()
     }
+
+    // Random number generator.
+    private val mGenerator = Random()
+
+    val randomNumber: Int
+        get() = mGenerator.nextInt(100)
+
+
+    override fun onBind(intent: Intent): IBinder {
+       // TODO("Return the communication channel to the service.")
+        return binder
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        // All clients have unbound with unbindService()
+        Log.d("PinkService", "On Unbind Called" )
+        return allowRebind
+    }
+
+    override fun onRebind(intent: Intent) {
+        // A client is binding to the service with bindService(),
+        // after onUnbind() has already been called
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("PinkService", "On Create Called" )
+        Toast.makeText(this, "On Create Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("PinkService", "On Start Command" )
+        Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show()
+        thread(start = true){
+            while (true) {
+                Log.d("PinkService", "Logging Message")
+                Thread.sleep(1000)
+            }
+        }
+        startLoggerForegroundServices()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("PinkService", "On Destroy Called" )
+        Toast.makeText(this, "On Destroy Called", Toast.LENGTH_SHORT).show()
+    }
+
+
+    fun startLoggerForegroundServices(){
+        createNotificationChannel()
+        val notification = createNotification()
+        Log.d("PinkService", "Foreground Service Started" )
+        startForeground(111, notification)
+    }
+
+    fun getPendingIntent(): PendingIntent{
+        val serviceIntent = Intent(this, PinkService::class.java)
+        return PendingIntent.getActivity(this,0,serviceIntent, PendingIntent.FLAG_IMMUTABLE)
+    }
+
+    fun createNotificationChannel(): NotificationChannel {
+        val channel = NotificationChannel("ID","PINKSERVICE", NotificationManager.IMPORTANCE_DEFAULT)
+        val notificationManager = ContextCompat.getSystemService(this, NotificationManager::class.java)
+        notificationManager!!.createNotificationChannel(channel)
+        return channel
+    }
+
+    fun createNotification():Notification{
+            var notification = NotificationCompat.Builder(this, "ID")
+                .setContentText("Foreground Pink Service Running")
+                .setContentTitle("PinkServices")
+                .setContentIntent(getPendingIntent())
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setOngoing(true)
+                .build()
+            return notification
+    }
+
+
+
+    inner class PinkServiceBinder: Binder() {
+        fun getService(): PinkService = this@PinkService
+    }
+
 }
